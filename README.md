0089b61143b7ee8259a92ca78ed32e41da79ee5f
Jean Carlos Antonio Pereira <jean-bra@protonmail.com>
2022-08-27 17:45:09 -0300

    Add tipos em typescript

--- README.md
+++ README.md
@@ -14,6 +14,6 @@ Para gerar o arquivo javascript a partir do arquivo typescript, executar o coman
 
 `tsc basico.ts`
 
-Este comando criará o arquivo basico.js.
+Este comando criará o arquivo `basico.js`.
 
 Para não precisar digitar o comando acima a todo momento, podemos utilizar o comando `tsc -w` no diretório raiz, isso fará com que o typescript monitore os arquivos `.ts` e compile para javascript sempre que eles sofrerem alterações.
---
+++ tipos/tipos.ts
@@ -0,0 +1,15 @@
+// string
+let nome = 'Jean Carlos';
+console.log(nome);
+// nome = 42; Gera erro, pois quando a variável é inicializada como string, o tipo é inferido à variável, não permitindo a troca de tipo.
+
+// number
+let idade = 42;
+// idade = 'Jean Carlos'; Gera erro pelo mesmo motivo descrito acima.
+idade = 42.5; // Permitido, assim como o JavaScript, o Typescript não faz diferenciação entre inteiro e ponto flutuante, é tudo Number.
+console.log(idade);
+
+// boolean
+let possuiHobbies = false;
+// possuiHobbies = 1; Gera erro pelo mesmo motivo descrito acima.
+console.log(possuiHobbies);
